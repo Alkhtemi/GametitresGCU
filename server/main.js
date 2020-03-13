b12b0d684516427ad5b449d49282da495896e05b
Moh <47746772+Alkhtemi@users.noreply.github.com>
2020-03-13 10:24:10 +0000

    port change

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ const WebSocketServer = require('ws').Server;
 const Session = require('./session');
 const Client = require('./client');
 
-const server = new WebSocketServer({port: 9000});
+const server = new WebSocketServer({port: 8080});
 
 const sessions = new Map;
 
